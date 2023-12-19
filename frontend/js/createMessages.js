d64c5e0252508f6eb33fcdf7c7b57e247a80f1c4
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-12-19 01:09:11 -0200

    websockets funcionando

---
+++ frontend/js/createMessages.js
@@ -0,0 +1,27 @@
+export const messageSelfElement = (content) => {
+
+    const element = document.createElement('div')
+
+    element.classList.add('messages-self')
+    element.innerHTML = content
+
+    return element
+}
+
+export const messageOthersElement = (content, sender, senderColor) => {
+
+    const element = document.createElement('div') 
+    const span = document.createElement('span') 
+
+    element.classList.add('messages-others')
+    span.classList.add('messages-sender')
+
+    span.style.color = senderColor
+
+    element.appendChild(span)
+
+    span.innerHTML = sender
+    element.innerHTML += content
+
+    return element
+}
--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,1 +1,105 @@
-console.log("tesntando conexão")
+import { messageSelfElement, messageOthersElement } from "./createMessages.js";
+
+'use strict';
+// login elements
+
+const login = document.querySelector('.login');
+const login_form = login.querySelector('.login__form');
+const login_input = login.querySelector('.login__input');
+
+
+//  chat elements
+
+const chat = document.querySelector('.chat');
+const chat_form = chat.querySelector('.chat__form');
+const chat_input = chat.querySelector('.chat__input');
+const chat_field = chat.querySelector('.chat__messages');
+
+
+// user
+const User = { id: '', name: '', color: '' };
+
+const colors = [
+    'red',
+    'palevioletred',
+    'olivedrab',
+    'orangered',
+    'orchid',
+    'blueviolet',
+    'royalblue',
+    'rebeccapurple',
+    'salmon',
+    'springgreen',
+    'khaki'
+];
+
+let websocket;
+
+const setRandomColor = () => {
+
+    const index = Math.floor(Math.random() * colors.length)
+    return colors[index]
+}
+
+// desliza a tela para o final da janela
+const scrollscreen = () => {
+
+    window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: "smooth"
+    })
+}
+
+// processa o conteudo enviado pelo servidor
+// definindo se a mensagem é do proprio usuario ou de outro.
+const processMessage = ({ data }) => {
+
+    const { userId, userName, userColor, content} = JSON.parse(data)
+
+    const message = userId == User.id 
+    ? messageSelfElement(content) 
+    : messageOthersElement(content, userName, userColor);
+
+    chat_field.appendChild(message)
+
+    scrollscreen()
+}
+
+// assim que o login for concluido, cria o elemento User e o conecta ao servidor
+const handleLogin = (event) => {
+
+    event.preventDefault()
+
+    User.id = crypto.randomUUID()
+    User.name = login_input.value
+    User.color = setRandomColor()
+
+    login.style.display = 'none'
+    chat.style.display = 'flex'
+
+    websocket = new WebSocket("ws://localhost:8080")
+    websocket.onmessage = processMessage
+
+    console.log(User)
+}
+
+
+// envia ao servidor um objeto com os dados do usuario e sua mensagem
+const sendMessage = (event) => {
+
+    event.preventDefault()
+
+    const message = {
+        userId: User.id,
+        userName: User.name,
+        userColor: User.color,
+        content: chat_input.value
+    }
+
+    websocket.send(JSON.stringify(message))
+
+    chat_input.value = ''
+}
+
+login_form.addEventListener('submit', handleLogin)
+chat_form.addEventListener('submit', sendMessage)
