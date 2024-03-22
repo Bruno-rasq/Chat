2db8d33c233c7c4ccb3bce21a66b16919669fb76
Bruno rasq <brunodiasrasquinha@gmail.com>
2024-03-21 23:22:37 -0300

    change server url

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -119,7 +119,7 @@ const handleLogin = (event) => {
     login.style.display = 'none'
     chat.style.display = 'flex'
 
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("wss://chat-ehja.onrender.com")
     websocket.onmessage = processMessage
 
     userConnected()
